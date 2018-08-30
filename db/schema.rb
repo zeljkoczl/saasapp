fa6efedbda848994fa784f70da997582b8485309
Zeljko Colovic <zeljkoczl@gmail.com>
2018-08-30 16:39:26 +0000

    Added profiles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170114235950) do
+ActiveRecord::Schema.define(version: 20180830163914) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -28,19 +28,6 @@ ActiveRecord::Schema.define(version: 20170114235950) do
   end
 
   create_table "profiles", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "job_title"
-    t.string   "phone_number"
-    t.string   "contact_email"
-    t.text     "description"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
